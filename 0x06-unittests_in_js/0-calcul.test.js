bce4341f3006d7569bcd194e8cfe614b1d82cc1c
Sharon <Sharonwanjiru392@gmail.com>
2024-02-29 21:10:36 -0800

    0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,16 +1,36 @@
-const calculateNumber = require("./0-calcul.js");
-const mocha = require('mocha');
-const assert = require("assert");
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-    it('returns rounded sum', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1.6, 3), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.8), 5);
-    assert.strictEqual(calculateNumber(-1, -3), -4);
-    assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
-    });
-    it('should throw error if NaN passed', function () {
-        assert.throws(() => calculateNumber(NaN, 3), '[Function: TypeError]');
-    });
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+  });
+
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+  });
+
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+  });
+
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  });
+
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
+
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
+  });
 });
